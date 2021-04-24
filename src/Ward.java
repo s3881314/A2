2c38fa6f7b6a549944a6d5bff69276fccc4bbce7
Warrant <warrant1997@gmail.com>
2021-04-25 09:17:38 +1000

    Follow sample class model

--- src/Ward.java
+++ src/Ward.java
@@ -1,9 +1,2 @@
-public class Ward {
-    private String ID;
-    private int NumOfBed = 0;
-
-    Ward(String id, int n){
-        this.ID = id;
-        this.NumOfBed = n;
-    }
+package PACKAGE_NAME;public class Ward {
 }
