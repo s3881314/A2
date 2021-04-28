1209734e6b7bb3675f4c76e17f67e695e92cbf38
Warrant <warrant1997@gmail.com>
2021-04-28 17:30:54 +1000

    Still thinking...

--- src/MedicalStaff.java
+++
@@ -1,9 +0,0 @@
-public abstract class MedicalStaff{
-    protected abstract void ModifyPassword(String id, String np); // Staff ID, new Password
-    protected abstract void ModifyID(String id);  // Staff ID
-    // nurses are assigned to two shifts from 8am to 4 pm, and 2 pm to 10 pm. No more than 8 hours per day
-    // doctors are assigned for 1 hour
-    protected abstract void AssignShift(String id, String sft);  // Staff ID, Shift
-    protected abstract void ModifyShift(String id, String sft);  // Staff id, Shift
-
-}
--- src/NonMedicalStaff.java
+++
@@ -1,7 +0,0 @@
-public abstract class NonMedicalStaff {
-    // Add new member
-    protected abstract void AddDoctor(String id, String p); // Staff ID and Password
-    protected abstract void AddNurse(String id, String p); // Staff ID and Password
-    // Allow manager to add new residents to one of the vacant beds
-    protected abstract void AddNewResidentToBed(String id,String g, String b, String w); // Resident ID, ,Gender, Bed ID, Ward ID
-}
